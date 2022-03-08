cfc9ccd8a2bb26224a3d4b84c47241b2f088be1c
Jeremy Washington <thebeluvdtrex@gmail.com>
2022-03-08 14:41:53 -0800

    Hopefully this works.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import Animal.Dog;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Dog dogRex = new Dog();
+        dogRex.speak();
+	dogRex.speak();
+    }
+
+
+
+
+}
